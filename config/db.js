4268c24990bc22b311ea5ec3521c78410ae8714b
OlegSvynarchuk <zhelibon@gmail.com>
2021-05-08 14:49:36 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@
 
 const mongoose = require('mongoose')
 require('dotenv').config()
-const url = process.env.DB_URL || 'mongodb+srv://oleg:svynarchuk@cluster0-ygmag.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const url = process.env.DB_URL
 
 
 

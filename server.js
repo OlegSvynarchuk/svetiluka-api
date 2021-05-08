e2f3ca85eaf9d239e600e530d37f57d70c8fee40
OlegSvynarchuk <zhelibon@gmail.com>
2021-05-08 16:47:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.use((error, req, res, next) => {
     })
 })
 
-const port = process.env.port ||  3001
-app.listen(port, function() {console.log(`app started on port ${port}`)})
+const port = process.env.PORT ||  3001
+app.listen(PORT, function() {console.log(`app started on port ${PORT}`)})
